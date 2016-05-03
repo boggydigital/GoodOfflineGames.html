91156abdd291c6447f86e60e434a1082daf53453
Bogdan Brinza <bogdan.brinza@gmail.com>
2016-05-02 21:48:32 -0700

    searchController plumbing

--- _source/searchController.ts
+++ _source/searchController.ts
@@ -1,8 +1,25 @@
-export class SearchController {
-    
-    public constructor(searchInput: HTMLInputElement) {
-        searchInput.addEventListener("input", function(e) {
-            console.log(searchInput.value);
-        });
+import {SearchViewModel} from "./viewModel/searchViewModel";
+import {ISearchViewModelProvider} from "./viewModel/searchViewModelProvider";
+
+export class SearchController<T> {
+
+    searchViewModelProvider: ISearchViewModelProvider<T>;
+    searchIndex: Array<SearchViewModel>;
+
+    public constructor(searchViewModelProvider: ISearchViewModelProvider<T>) {
+        this.searchViewModelProvider = searchViewModelProvider;
+        this.searchIndex = new Array<SearchViewModel>();
     }
+    
+    public index = (items: Array<T>) => {
+        for (let ii=0; ii<items.length; ii++) {
+            this.searchIndex.push(this.searchViewModelProvider.getViewModel(items[ii]));           
+        }
+    }
+    
+    // public constructor(searchInput: HTMLInputElement) {
+    //     searchInput.addEventListener("input", function(e) {
+    //         console.log(searchInput.value);
+    //     });
+    // }
 }
